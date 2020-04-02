70b883b1ced0248199bc95c19e99a32b1da0e012
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-02 16:30:43 +0530

    FirstCommit

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace OurProduct.Models
+{[Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PDesc { get; set; }
+        public int PPrice { get; set; }
+    }
+}
